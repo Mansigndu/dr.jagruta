88df6c54ea6552a65cfbefe7eca46a0f09ae1c7f
Mansigndu <mansi.saini2412@gmail.com>
2026-06-11 20:55:03 +0530

    do some changes

--- footer.js
+++ footer.js
@@ -119,7 +119,7 @@ footer.innerHTML = `
 
 <a href="https://wa.me/918329242513"
  target="_blank"
- class="fixed bottom-6 right-6 w-14 h-14 sm:w-16 sm:h-16 bg-green-500 rounded-full flex items-center justify-center text-white text-2xl sm:text-3xl shadow-2xl z-50 hover:scale-110 transition duration-300">
+ class="fixed bottom-16 right-8 w-14 h-14 sm:w-16 sm:h-16 bg-green-500 rounded-full flex items-center justify-center text-white text-2xl sm:text-3xl shadow-2xl z-50 hover:scale-110 transition duration-300">
 
 
 💬
